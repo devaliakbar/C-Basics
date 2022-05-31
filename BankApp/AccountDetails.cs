cda564b4a203a433318f64bdf7e9ec70e26596ad
devaliakbar <dev.aliakbar@gmail.com>
2022-05-31 21:47:57 +0200

    Main features added

---
+++ BankApp/AccountDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace BankApp
+{
+    public class AccountDetails
+    {
+        public int accountNumber;
+        public string accountName;
+        public DateTime birthday;
+        public double balance = 0;
+
+        public AccountDetails(int mAccountNumber, string mAccountName, DateTime mBirthday)
+        {
+            accountNumber = mAccountNumber;
+            accountName = mAccountName;
+            birthday = mBirthday;
+        }
+    }
+}
+
